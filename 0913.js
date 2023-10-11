8f105b8dcf3b92fef2f4ae693035bc4ece704441
tnqlsrkd <133744542+tnqlsrkd@users.noreply.github.com>
2023-10-11 14:34:40 +0900

    choisubin
    java

---
+++ 0913.js
@@ -0,0 +1,25 @@
+let korea_name1 = "윤누리"
+let korea_birth1 = 2004
+let korea_gender1 = "w"
+ 
+let korea_name2 = "___"
+let korea_birth2 = 2003
+let korea_gender2 = "w"
+ 
+let korea_name3 = "---"
+let korea_birth3 = 2004 
+let korea_gender3 = "w"
+ 
+let korea_name4 = "+++"
+let korea_birth4 = 2003
+let korea_gender = "w"
+ 
+let korea_name5 = "==="
+let korea_birth5 = 2004
+let korea_gender5 = "w"
+ 
+sum_age = korea_birth1 + korea_birth2 + korea_birth3 + korea_birth4 + korea_birth5;
+avg_age = sum_age / 5;
+
+console.log(sum_age)
+console.log(avg_age)
